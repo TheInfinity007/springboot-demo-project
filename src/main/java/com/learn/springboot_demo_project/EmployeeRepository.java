c80c6ee91e1542b76df5e627c181e0fc1ec8c17a
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-08-24 01:51:51 +0530

    Add employee repository interface for crud with DB

---
+++ src/main/java/com/learn/springboot_demo_project/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.learn.springboot_demo_project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
