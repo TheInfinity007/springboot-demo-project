48fd7f4ec28b878fa15c7cd4c69babadc2dd8fa9
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-08-24 01:55:01 +0530

    remove the public keyword from interface

--- src/main/java/com/learn/springboot_demo_project/EmployeeRepository.java
+++ src/main/java/com/learn/springboot_demo_project/EmployeeRepository.java
@@ -2,5 +2,5 @@ package com.learn.springboot_demo_project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
