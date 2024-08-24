7e4572de3da856878727376e1891dcef4c4f4ec5
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2024-08-25 02:30:44 +0530

    create cancel and complete order routes and add links when order is in progress state

--- src/main/java/com/learn/springboot_demo_project/OrderController.java
+++ src/main/java/com/learn/springboot_demo_project/OrderController.java
@@ -3,6 +3,10 @@ package com.learn.springboot_demo_project;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -64,5 +68,50 @@ public class OrderController {
         EntityModel<Order> data = assembler.toModel(newOrder);
 
         return ResponseEntity.created(data.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(data);
+    }
+
+    @DeleteMapping("/orders/{id}/cancel")
+    ResponseEntity<?> cancel(@PathVariable Long id) {
+        Order order = repository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.CANCELLED);
+            order = repository.save(order);
+
+            EntityModel<Order> orderModel = assembler.toModel(order);
+            return ResponseEntity.ok().body(orderModel);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(
+                        Problem.create()
+                                .withTitle("Method Not Allowed")
+                                .withDetail("You can't cancel an order that is in the " + order.getStatus() + " status")
+                );
+    }
+
+    @PutMapping("/orders/{id}/complete")
+    ResponseEntity<?> complete(@PathVariable Long id) {
+        Order order = repository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException(id));
+
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            order.setStatus(Status.COMPLETED);
+            order = repository.save(order);
+
+            EntityModel<Order> orderEntity = assembler.toModel(order);
+            return ResponseEntity.ok().body(orderEntity);
+        }
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+                .body(
+                        Problem.create()
+                                .withTitle("Method Not Allowed")
+                                .withDetail("You can't complete an order that is in the " + order.getStatus() + " status")
+                );
     }
 }
--- src/main/java/com/learn/springboot_demo_project/OrderModelAssembler.java
+++ src/main/java/com/learn/springboot_demo_project/OrderModelAssembler.java
@@ -12,7 +12,9 @@ public class OrderModelAssembler implements RepresentationModelAssembler<Order, 
 
     @Override
     public EntityModel<Order> toModel(Order order) {
-        return EntityModel.of(order,
+
+        // Unconditional links for single-item resources and aggregate root
+        EntityModel<Order> orderModel = EntityModel.of(order,
                 linkTo(
                         methodOn(OrderController.class)
                                 .one(order.getId())
@@ -20,7 +22,26 @@ public class OrderModelAssembler implements RepresentationModelAssembler<Order, 
                 linkTo(
                         methodOn(OrderController.class)
                                 .all()
-                ).withRel("Orders")
+                ).withRel("orders")
         );
+
+
+        // Conditional links based on the status of the order
+        if (order.getStatus() == Status.IN_PROGRESS) {
+            orderModel.add(
+                    linkTo(
+                            methodOn(OrderController.class)
+                                    .cancel(order.getId())
+                    ).withRel("cancel")
+            );
+            orderModel.add(
+                    linkTo(
+                            methodOn(OrderController.class)
+                                    .complete(order.getId())
+                    ).withRel("complete")
+            );
+        }
+
+        return orderModel;
     }
 }
